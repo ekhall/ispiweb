f3f83de238cbfd323f2f556751de4d08efe65d67
Archetyp Mobility <kevin@archetyp.io>
2016-09-09 13:01:53 -0400

    More participants

--- src/components/participants.js
+++ src/components/participants.js
@@ -9,23 +9,43 @@ export default class Participants extends Component {
             <h1 className="ui header">Participating Institutions</h1>
                   <div className="ui grid">
                     <div className="four wide column">
-                      <img className="participant" src="/assets/participants/choc.png" />
-                    </div>
-                    <div className="four wide column">
                       <img className="participant" src="/assets/participants/bambinogesu.png" />
                     </div>
                     <div className="four wide column">
                       <img className="participant" src="/assets/participants/choc.png" />
                     </div>
                     <div className="four wide column">
-                      <img className="participant" src="/assets/participants/bambinogesu.png" />
+                      <img className="participant" src="/assets/participants/miami.svg" />
+                    </div>
+                    <div className="four wide column">
+                      <img className="participant" src="/assets/participants/stanford.png" />
+                    </div>
+
+                    <div className="two wide column"></div>
+                    <div className="four wide column">
+                      <img className="participant" src="/assets/participants/pfizer.png" />
+                    </div>
+                    <div className="four wide column column-middle">
+                      <img className="participant" src="/assets/participants/sickkids.png" />
+                    </div>
+                    <div className="four wide column">
+                      <img className="participant" src="/assets/participants/rb.png" />
+                    </div>
+                    <div className="two wide column"></div>
+
+                    <div className="four wide column">
+                      <img className="participant" src="/assets/participants/ucsf.png" />
                     </div>
                     <div className="four wide column">
                       <img className="participant" src="/assets/participants/choc.png" />
                     </div>
                     <div className="four wide column">
-                      <img className="participant" src="/assets/participants/bambinogesu.png" />
+                      <img className="participant" src="/assets/participants/miami.svg" />
                     </div>
+                    <div className="four wide column">
+                      <img className="participant" src="/assets/participants/stanford.png" />
+                    </div>
+
                   </div>
           </div>
         </div>
